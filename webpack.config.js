fc53871628882bf4f1fa565630f43ddfcce00c4c
aishuuu1020 <aishwaryasonawane191627@gmail.com>
2025-04-27 14:27:59 +0530

    Added code of about us

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         static:{
             directory: path.resolve(__dirname, 'dist')
         },
-        port: 3001,
+        port: 3008,
         open: true,
         hot: true,
         compress: true,
